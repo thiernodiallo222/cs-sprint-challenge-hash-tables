e3ed93e3f280b7a58498332257bace21448e9a16
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-08-18 04:15:11 -0400

    just commit

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,42 +1,18 @@
-# dict ={}
-# def single_list(arr):
-#     for i in range(len(arr)):
-#         dict[i]=arr[i]
-#     return dict 
-
-
+ 
 def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    result = []
-    for array in arrays:
-        for arr in array:
-            result1[arr] = arr
-            if result1[arr] == arr:
-                result.append(arr)
+    dict_arrays={}
+    for index in range (len(arrays)):
+        dict_arrays[i]=arrays[i]
 
-    return result
-    # dict_arrays={}
-    # for index in range (len(arrays)):
-    #     dict_arrays[i]=arrays[i]
+    first = arrays[0] 
 
-    # first = arrays[0] 
-
-    # for i in 
+    for i in range()
         
 
-    # dict2={}
-    # first=arrays[0]
-    # result = []
-    # for i in range(len(arrays)):
-    #     dict2[i]=arrays[i] 
-
-    # for i in range(len(first)):
-    #     for j in range(len(arrays)):
-    #         if first[i] in arrays[j]:
-    #             result.append(first[i])
-    # return result    
+     
 
 
         
